d11146389bd7806b1049705242bcb0216a4a4660
Anthony Njuguna <tonju2@yahoo.com>
2021-01-17 17:53:57 -0600

    Second version

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,15 +6,47 @@ budgetcsv = os.path.join("Resources", "budget_data.csv")
 
 totalmonths = 0
 nettotal = 0
+avgchange = 0
+
+totalprofit = 0
+totalloss - 0
+nettotal = 0
+profit = 0
+
+
+
 
 # Open and read csv
-with open(budgetcsv) as csvfile:
+with open(budgetcsv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    # print(csvreader)
     # skip header
     header = next(csvreader)
-    for counter in csvfile:
+
+    
+
+    for row in csvreader:
         totalmonths += 1
-        nettotal += float(csvfile[1])
-print(totalmonths, nettotal)
+        nettotal = sum(int(row[1])
+        
+        profit = int(row[1])
+         
+        if profit > 0:
+            totalprofit += profit
+        elif profit < 0:
+            totalloss += profit
+            
+    nettotal = totalprofit - totalloss
+    avgchange = (nettotal - totalloss)/totalmonths
+    maxprofit = 0
+    maxprofit = max(int(row[1]]))
+    minprofit = 0
+    minprofit = min(int(row[1]))
+   
+print(totalmonths)
+print(nettotal)
+print(avgchange)
+print(maxprofit)
+print(minprofit)
 
